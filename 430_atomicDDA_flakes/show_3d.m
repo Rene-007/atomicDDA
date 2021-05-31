02bfdacff40be63a161df5c49eb2f2072a3cfd43
rene-007 <kullock@gmx.de>
2021-05-31 14:12:09 +0200

    better image

--- 430_atomicDDA_flakes/show_3d.m
+++ 430_atomicDDA_flakes/show_3d.m
@@ -4,7 +4,7 @@ function show_3d(r0,r_on)
     figure;
     % r1 = r0;
     r1 = r0(r_on,:);
-    scatter3(r1(:,1),r1(:,2),r1(:,3),30,'MarkerEdgeColor','k', 'MarkerFaceColor',[1 0.9 0]); 
+    scatter3(r1(:,1),r1(:,2),r1(:,3),15,'MarkerEdgeColor','k', 'MarkerFaceColor',[1 0.9 0]); 
     axis equal; axis tight;
     rotate3d on;
 
